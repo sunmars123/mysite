3a61ba853eff6db61d584c46cca2ef9796f84294
sunmars123 <17673637740@163.com>
2018-06-13 19:05:27 +0800

    增加了list学习

---
+++ day02/list_slice.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2018/6/5 16:35 
+# @Author : Mars
+# @Site :  
+# @File : list_slice.py 
+# @Software: PyCharm
+# 定义一个列表
+a = [1, 2, 3, 3, 2, 6, 7]
+# 增加操作 append insert
+# 追加 append
+# a.append(8)
+# # 插入 insert
+# a.insert(0, 0)
+# print(a)
+# 合并列表extend
+# b = [10, 11]
+# a.extend(b)
+# print(a)
+# # 修改操作
+# a[0] = 10
+# print(a)
+# # 删除元素
+# pop通过坐标删除并返回这个元素 remote移除这个元素
+# a.remove(10)
+# b = a.pop(0)
+# print(a)
+# print(b)
+# 清除所有元素 clear
+# a.clear()
+# print(a)
+# count函数，查找某个元素出现的个数
+# i = a.count(2)
+# print(i)
+# 反转函数 reverse
+a.reverse()
+print(a)
